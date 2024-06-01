28b50d4b01dd1f0e6a663c369aed9e336b67646b
snoopy <snoopy_zj@163.com>
2024-06-01 09:31:04 +0000

    add test

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
 # TODO add test cases
 
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-test", action="store_true", help="Whether to freeze the feature encoder")
-
-args = parser.parse_args()
-
-if args.test:
-    print("test")
-
