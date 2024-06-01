8441866b989ca65afc0e7031a7ee02d9a466df4b
snoopy <snoopy_zj@163.com>
2024-06-01 08:41:40 +0000

    add grl model

---
+++ model/wav2vec2_layer.py
@@ -0,0 +1,98 @@
+from typing import Optional, Tuple, Union
+from transformers import Wav2Vec2PreTrainedModel, Wav2Vec2Model
+import torch
+from torch import nn
+
+_HIDDEN_STATES_START_POSITION = 2
+
+class Wav2Vec2LayerClassification(Wav2Vec2PreTrainedModel):
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(config)
+
+        if hasattr(config, "add_adapter") and config.add_adapter:
+            raise ValueError(
+                "Sequence classification does not support the use of Wav2Vec2 adapters (config.add_adapter=True)"
+            )
+        self.wav2vec2 = Wav2Vec2Model(config)
+        num_layers = config.num_hidden_layers + 1  # transformer layers + input embeddings
+        if config.use_weighted_layer_sum:
+            self.layer_weights = nn.Parameter(torch.ones(num_layers) / num_layers)
+        self.projector = nn.Linear(config.hidden_size, config.classifier_proj_size)
+        self.classifier = nn.Linear(config.classifier_proj_size, config.num_labels)
+        
+        self.loss_fct = nn.CrossEntropyLoss(label_smoothing=0.25)
+        
+
+        # Initialize weights and apply final processing
+        self.post_init()
+    
+    def freeze_layers(self, num_layers_to_freeze: int):
+        for param in self.wav2vec2.encoder.layers[num_layers_to_freeze:].parameters():
+            param.requires_grad = False
+    
+    def freeze_feature_encoder(self):
+        """
+        Calling this function will disable the gradient computation for the feature encoder so that its parameter will
+        not be updated during training.
+        """
+        self.wav2vec2.feature_extractor._freeze_parameters()
+        
+    def freeze_base_model(self):
+        """
+        Calling this function will disable the gradient computation for the base model so that its parameters will not
+        be updated during training. Only the classification head will be updated.
+        """
+        for param in self.wav2vec2.parameters():
+            param.requires_grad = False
+            
+    def forward(
+        self,
+        input_values: Optional[torch.Tensor],
+        attention_mask: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        labels: Optional[torch.Tensor] = None,
+        use_layer: Optional[int] = None,
+    ) -> Tuple:
+        
+        output_hidden_states = True if self.config.use_weighted_layer_sum else output_hidden_states
+
+        outputs = self.wav2vec2(
+            input_values,
+            attention_mask=attention_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=True,
+        )
+        
+        if self.config.use_weighted_layer_sum:
+            hidden_states = outputs[_HIDDEN_STATES_START_POSITION]
+            hidden_states = torch.stack(hidden_states, dim=1)
+            norm_weights = nn.functional.softmax(self.layer_weights, dim=-1)
+            hidden_states = (hidden_states * norm_weights.view(-1, 1, 1)).sum(dim=1)
+        else:
+            hidden_states = outputs[0]
+
+        if use_layer is not None and len(outputs) == 3:
+            hidden_states = outputs[2][use_layer]
+        
+        
+        hidden_states = self.projector(hidden_states)
+        
+        if attention_mask is None:
+            pooled_output = hidden_states.mean(dim=1)
+        else:
+            padding_mask = self._get_feature_vector_attention_mask(hidden_states.shape[1], attention_mask)
+            hidden_states[~padding_mask] = 0.0
+            pooled_output = hidden_states.sum(dim=1) / padding_mask.sum(dim=1).view(-1, 1)
+
+        logits = self.classifier(pooled_output)
+
+        loss = None
+        if labels is not None:
+            loss = self.loss_fct(logits.view(-1, self.config.num_labels), labels.view(-1))
+
+        output = (logits,) + outputs[_HIDDEN_STATES_START_POSITION:]
+        return ((loss,) + output) if loss is not None else output
+
+    
