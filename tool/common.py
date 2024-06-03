036e424c4d6103ae9f0eff02be76052300f31248
snoopy <snoopy_zj@163.com>
2024-06-03 02:46:22 +0000

    add compute model parameter

---
+++ tool/common.py
@@ -0,0 +1,4 @@
+def get_model_parameter(model):
+    total_params = sum(p.numel() for p in model.parameters())
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return total_params, trainable_params
