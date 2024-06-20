67ac49f5a8af8d25c4f02c103e4a628865022df7
snoopy <snoopy_zj@163.com>
2024-06-20 06:42:57 +0000

    feat: Add analysis scripts for Wav2Vec2 model

---
+++ analysis.py
@@ -0,0 +1,85 @@
+import torch
+from transformers import AutoFeatureExtractor, Wav2Vec2ForSequenceClassification
+from torch.utils.data import DataLoader
+import os
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+from module.mydatasets import MyDataset
+
+root_file = f"./analysis_res/wav2vec2-base-Fcnn-SL/"
+model_path = "./exp/wav2vec2-base-Fcnn-SL/checkpoint-23704"
+dataset_path = "/root/DialectDataset/Datatang-Dialect"
+dataset = MyDataset(manifest_path=os.path.join("./data","dev.tsv"), dataset_path=dataset_path)
+
+with open(os.path.join(root_file, "label2dialect"), "a", encoding="utf-8") as f:
+    for key,value in dataset.labels_dict.items():
+        f.write(f"{value} {key}\n")
+
+feature_extractor = AutoFeatureExtractor.from_pretrained(model_path)
+model = Wav2Vec2ForSequenceClassification.from_pretrained(model_path, num_labels=len(dataset.labels_dict))
+
+model.eval()
+
+def collate_fn(batch):
+    """
+    batch -> speech_feature, input_lengths, label, sex, speaker
+    Return:
+        inputs : [batch, max_time, dim]
+        input_lengths: [batch]
+        label: [batch]
+        sex: [batch]
+        speaker: [batch]
+    """
+    speech_feature = [i[0].numpy() for i in batch]
+    label = torch.LongTensor([i[1] for i in batch])
+    speaker = torch.LongTensor([i[2] for i in batch])
+
+    # TODO add FBANK and MFCC feature
+
+    speech_feature = feature_extractor(
+        speech_feature, 
+        sampling_rate=feature_extractor.sampling_rate,
+        padding=True,
+        return_tensors="pt"
+    )
+
+    return {
+            "input_values": speech_feature["input_values"],
+            "labels": label,
+            "speaker_labels": speaker,
+        }
+
+dataloaders = DataLoader(dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
+
+# 记录模型权重
+with open(os.path.join(root_file, "layer_weights"), "a", encoding="utf-8") as f:
+    f.write(f"{torch.nn.functional.softmax(model.layer_weights, dim=-1).detach()}\n")
+
+for batch in tqdm(dataloaders):
+    outputs = model(input_values=batch["input_values"], labels=batch["labels"])
+
+    for idx in range(len(model.layer_weights)):
+        hidden_state = outputs.hidden_states[idx].detach() # [batch, time, dim]
+        layer_weight = model.layer_weights[idx].detach()
+
+        hidden_state = hidden_state * torch.nn.functional.softmax(layer_weight, dim=-1)
+        hidden_state = hidden_state.mean(dim=1)
+
+        # 写入到对应的文件中
+        path = os.path.join(root_file, f"layer_{12-idx}")
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        with open(os.path.join(path, "hidden_state.txt"), "a") as f:
+            f.write(f"{batch['labels'][0].detach()}\t{batch['speaker_labels'][0].detach()}\t{str(hidden_state.squeeze(0).detach().tolist())}\n")
+
+        
+
+
+        
+
+
---
+++ generate_analysis_res.py
@@ -0,0 +1,45 @@
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import ast
+from tqdm import tqdm
+
+def save_tsne_res(logits, labels, idx, state):
+    # 进行TSNE降维
+    tsne = TSNE(n_components=2, random_state=42, learning_rate=200)
+    tsne_result = tsne.fit_transform(logits)
+
+    # 绘制散点图
+    plt.figure()
+    plt.scatter(tsne_result[:, 0], tsne_result[:, 1], c=labels, cmap="viridis",s=3)
+    plt.title(f"T-SNE Layer {idx}")
+
+    # 保存图片
+    out_path = os.path.join(root_file, f"layer_{idx}", f"tsne_layer_{state}.png")
+    plt.savefig(out_path,dpi=300)
+    plt.close()
+
+if __name__ == "__main__":
+    root_file = f"./analysis_res/wav2vec2-base-Fcnn-SL/"
+
+    for idx in tqdm(range(13)):
+        path = os.path.join(root_file, f"layer_{idx}")
+
+        hidden_states = []
+        labels = []
+        speakers = []
+        with open(os.path.join(path, "hidden_state.txt"), "r") as f:
+            for line in f.readlines():
+                label, speaker, hidden_state = line.strip().split("\t")
+                hidden_states.append(np.array(ast.literal_eval(hidden_state)))
+                labels.append(int(label))
+                speakers.append(int(speakers))
+            
+        hidden_states = np.array(hidden_states)
+        labels = np.array(labels)
+        speakers = np.array(speakers)
+        save_tsne_res(hidden_states, labels, idx, "dialect")
+        save_tsne_res(hidden_states, speakers, idx, "dialect")
+
